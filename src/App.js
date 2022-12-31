f0ee5e94a1df565e5c5294152d2f9942bba4c378
anastasia <anyasirman@outlook.com>
2022-12-31 14:10:46 -0500

    added months days hours and seconds

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import React from 'react';
 function App() {
 
-  const [count, setCount] = useState(0)
+
+  let today = new Date();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  const [currentMonth, setCurrentMonth] = useState(12 - today.getMonth()-1)
+  const [currentday, setCurrentDay] = useState(31 - today.getDate())
+  const [currentHour, setCurrentHour] = useState(24 -today.getHours())
+  const [currentMinute, setCurrentMinute] = useState(60 - today.getMinutes())
+  const [currentSecond, setCurrentSecond] = useState(60 -today.getSeconds())
+  useEffect(()=> {
+    setCurrentSecond(60 - today.getSeconds())
+
+}, [setInterval(()=> 
+  setCurrentSecond(60 - today.getSeconds())
+, 1000)])
+
   return (
     <div className="App">
-   <h1>Conter</h1>
-   <p>{count}</p>
-   <button onClick={()=> setCount(prev => prev+=1)}>+</button>
-   <button onClick={()=> count > 0 && setCount(prev => prev-=1)}>-</button>
+   <h1>New Years count down!</h1>
+    <h3>Months till {currentMonth}</h3>
+    <h3>Days till {currentday} </h3>
+    <h3>hours till {currentHour}</h3>
+    <h3>minutes till {currentMinute}</h3>
+    <h3>Seconds till {currentSecond}</h3>
+    <button>new</button>
     </div>
   );
 }
